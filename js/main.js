aac897d8950d16629224b4d78f89bf51ee069657
Davrizio <davriziod@gmail.com>
2023-01-28 22:05:39 -0500

    started date sort

--- js/main.js
+++ js/main.js
@@ -66,48 +66,48 @@ function proPresenterActivePresentationStage(){
 
 const pcoAuth = 'd5ce11575a546bfd596581e14083607044349ebac03d15244cdd5b5d72422503:3c5e4d41c2ddc955299b63dd607dcf0648d02e5193fd1f03739cbabee8cf6f1c'
 
-/*module.exports{
-pcoPlans: async (req, res) => {
-  try {
-    let response = await fetch('https://api.planningcenteronline.com/services/v2/service_types/1/plans', {method:'GET', 
-    headers: {'Authorization': 'Basic ' + btoa('d5ce11575a546bfd596581e14083607044349ebac03d15244cdd5b5d72422503:3c5e4d41c2ddc955299b63dd607dcf0648d02e5193fd1f03739cbabee8cf6f1c')}});
-    let data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.log(err);
+async function pcoPlan() {
+	const response = await fetch(
+		'https://api.planningcenteronline.com/services/v2/service_types/285487/plans?order=-sort_date&per_page=40',
+		{
+			method: 'GET',
+			headers: {
+				'Authorization': 'Basic ' + btoa(pcoAuth)
+			}
+		}
+	);
+  if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const data = await response.json(); // Extracting data as a JSON Object from the response
+  console.log(data)
+  for(sort_date in data.data){
+    console.log(data.data[sort_date].attributes.sort_date)
   }
+  document.querySelector('#pcoDate').innerText = data.data[0].attributes.dates
+  document.querySelector('#pcoSeriesTitle').innerText = data.data[0].attributes.series_title
+  document.querySelector('#pcoPlanTitle').innerText = data.data[0].attributes.title
 }
 
-} */
-function pcoPlan(){
-  fetch(`https://api.planningcenteronline.com/services/v2/service_types/285487/plans?where[series_title]=Northland 2.0`, {method:'GET', 
-  headers: {'Authorization': 'Basic ' + btoa(pcoAuth)}})
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-      document.querySelector('#pcoDate').innerText = data.data[0].attributes.dates
-      document.querySelector('#pcoSeriesTitle').innerText = data.data[0].attributes.series_title
-      document.querySelector('#pcoPlanTitle').innerText = data.data[0].attributes.title
-    })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
-}
-
-function pcoPlanItems(){
-  fetch(`https://api.planningcenteronline.com/services/v2/service_types/285487/plans/62389948/items`, {method:'GET', 
-  headers: {'Authorization': 'Basic ' + btoa(pcoAuth)}})
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-      console.log(data)
-      let itemList = document.querySelector("#pco")
-      for(title in data.data){
-        console.log(title)
-        let li = document.createElement("li");  
-        li.textContent = data.data[title].attributes.title;        
+async function pcoPlanItems() {
+	const response = await fetch(
+		'https://api.planningcenteronline.com/services/v2/service_types/285487/plans/62389948/items',
+		{
+			method: 'GET',
+			headers: {
+				'Authorization': 'Basic ' + btoa(pcoAuth)
+			}
+		}
+	);
+  if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const data = await response.json();
+  let itemList = document.querySelector("#pco")
+  for(title in data.data){
+    let li = document.createElement("li");  
+        li.textContent = data.data[title].attributes.title;
+        li.className = data.data[title].attributes.item_type        
         itemList.appendChild(li);
-      }
-    })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
+  }
 }
